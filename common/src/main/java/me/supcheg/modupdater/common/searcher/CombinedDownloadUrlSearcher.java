08810ae477fb9611963aecd3863fede584e2bf5e
Supcheg <santos43241@gmail.com>
2022-06-20 00:44:28 +0300

    Added same Updater checks in CombinedDownloadUrlSearcher

--- common/src/main/java/me/supcheg/modupdater/common/searcher/CombinedDownloadUrlSearcher.java
+++ common/src/main/java/me/supcheg/modupdater/common/searcher/CombinedDownloadUrlSearcher.java
@@ -1,14 +1,18 @@
 package me.supcheg.modupdater.common.searcher;
 
+import me.supcheg.modupdater.common.Updater;
 import me.supcheg.modupdater.common.mod.Mod;
+import me.supcheg.modupdater.common.util.Util;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class CombinedDownloadUrlSearcher implements DownloadUrlSearcher {
 
+    private Updater updater;
     private final Set<DownloadUrlSearcher> downloadUrlSearcherSet;
 
     public CombinedDownloadUrlSearcher() {
@@ -16,6 +20,11 @@ public class CombinedDownloadUrlSearcher implements DownloadUrlSearcher {
     }
 
     public boolean add(@NotNull DownloadUrlSearcher searcher) {
+        if (this.updater == null) {
+            this.updater = searcher.getUpdater();
+        } else {
+            Util.validateSameUpdater(this, searcher);
+        }
         return downloadUrlSearcherSet.add(searcher);
     }
 
@@ -29,7 +38,14 @@ public class CombinedDownloadUrlSearcher implements DownloadUrlSearcher {
 
     @Nullable
     @Override
+    public Updater getUpdater() {
+        return updater;
+    }
+
+    @Nullable
+    @Override
     public String find(@NotNull Mod mod) {
+        Util.validateSameUpdater(this, mod);
         for (DownloadUrlSearcher downloadUrlSearcher : downloadUrlSearcherSet) {
             String result = downloadUrlSearcher.find(mod);
             if (result != null) {
@@ -38,5 +54,23 @@ public class CombinedDownloadUrlSearcher implements DownloadUrlSearcher {
         }
 
         return null;
+    }
+
+    @Override
+    public String toString() {
+        return "CombinedDownloadUrlSearcher{downloaders=" + downloadUrlSearcherSet + '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        CombinedDownloadUrlSearcher that = (CombinedDownloadUrlSearcher) o;
+        return Objects.equals(updater, that.updater) && Objects.equals(downloadUrlSearcherSet, that.downloadUrlSearcherSet);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(updater, downloadUrlSearcherSet);
     }
 }
